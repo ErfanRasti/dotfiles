ccd013100e82828da503344e9ffa44e6686b333c
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2024-07-23 01:16:28 +0330

    chore: Add fzf configuration files

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/erfan/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/erfan/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# source "$HOME/.fzf/shell/completion.bash"
+
+# Key bindings
+# ------------
+# source "$HOME/.fzf/shell/key-bindings.bash"
---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/erfan/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/erfan/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# source "$HOME/.fzf/shell/completion.zsh"
+
+# Key bindings
+# ------------
+# source "$HOME/.fzf/shell/key-bindings.zsh"
