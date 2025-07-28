f15333370c7f73f82dc9074ea585e2f4aec71052
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-28 19:44:10 +0330

    feat(zsh): introduce script to apply GNOME proxy settings

---
+++ zsh/set_proxy.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# setup proxy
+# Get GNOME proxy settings and apply them
+apply_gnome_proxy() {
+   local proxy_mode=$(gsettings get org.gnome.system.proxy mode)
+   if [[ "$proxy_mode" == "'manual'" ]]; then
+       local proxy_host=$(gsettings get org.gnome.system.proxy.http host | tr -d "'")
+       local proxy_port=$(gsettings get org.gnome.system.proxy.http port)
+       export http_proxy="http://$proxy_host:$proxy_port"
+       export https_proxy="http://$proxy_host:$proxy_port"
+       export ftp_proxy="http://$proxy_host:$proxy_port"
+       export socks_proxy="http://$proxy_host:$proxy_port"
+       export no_proxy="localhost,127.0.0.1"
+       # export no_proxy="localhost,  127.0.0.0/8,  ::1"
+   elif [[ "$proxy_mode" == "'none'" ]]; then
+       unset http_proxy https_proxy ftp_proxy socks_proxy no_proxy
+   fi
+}
+apply_gnome_proxy
+
+
