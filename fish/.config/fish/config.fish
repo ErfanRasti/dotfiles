e6f10e0c9b3a7c75356bd46965b46a7969dd6724
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-29 19:51:54 +0330

    feat: add fastfetch to the config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -56,7 +56,8 @@ end
 zoxide init fish | source
 
 # Welcome page with ascii
-ascii-image-converter ~/Pictures/MyImages/(ls ~/Pictures/MyImages/ | grep -E '\.(jpg|png)$' | shuf -n 1) -C -c
+# ascii-image-converter ~/Pictures/MyImages/(ls ~/Pictures/MyImages/ | grep -E '\.(jpg|png)$' | shuf -n 1) -C -c
+fastfetch
 
 # Supress fish welcoming message
 set fish_greeting
