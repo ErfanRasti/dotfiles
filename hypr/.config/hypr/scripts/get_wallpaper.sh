837e3a4266b183183ba115061ce9c4ede028e17c
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-01 20:04:47 +0330

    feat: add update_rofi_configs.sh

--- hypr/.config/hypr/scripts/get_wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WALLPAPER=$(jq -r '."wallpaper.image"' ~/.config/hyprpanel/config.json 2>/dev/null || grep -oP '"wallpaper.image":\s*"\K[^"]+' ~/.config/hyprpanel/config.json)
-
-echo "$WALLPAPER"
