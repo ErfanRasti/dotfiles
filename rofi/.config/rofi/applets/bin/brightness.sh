46f3892d933a68292934747d7cabdb97513872ce
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-05 11:57:55 +0330

    fix(rofi): correct brightness adjustment options

--- rofi/.config/rofi/applets/bin/brightness.sh
+++ rofi/.config/rofi/applets/bin/brightness.sh
@@ -5,17 +5,17 @@ source "$HOME"/.config/rofi/applets/shared/theme.bash
 theme="$type/$style"
 
 # Brightness Info
-backlight="$(printf "%.0f\n" `light -G`)"
-card="`light -L | grep 'backlight' | head -n1 | cut -d'/' -f3`"
+backlight="$(printf "%.0f\n" $(light -G))"
+card="$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)"
 
 if [[ $backlight -ge 0 ]] && [[ $backlight -le 29 ]]; then
-    level="Low"
+  level="Low"
 elif [[ $backlight -ge 30 ]] && [[ $backlight -le 49 ]]; then
-    level="Optimal"
+  level="Optimal"
 elif [[ $backlight -ge 50 ]] && [[ $backlight -le 69 ]]; then
-    level="High"
+  level="High"
 elif [[ $backlight -ge 70 ]] && [[ $backlight -le 100 ]]; then
-    level="Peak"
+  level="Peak"
 fi
 
 # Theme Elements
@@ -30,51 +30,51 @@ win_width='550px'
 option_1="󰛨"
 option_2="󱠂"
 option_3="󱩎"
-option_4="󱧣 "
+option_4="󱧣"
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "window {width: $win_width;}" \
-		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
+  rofi -theme-str "window {width: $win_width;}" \
+    -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+    -theme-str 'textbox-prompt-colon {str: "";}' \
+    -dmenu \
+    -p "$prompt" \
+    -mesg "$mesg" \
+    -markup-rows \
+    -theme ${theme}
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		brightnessctl set 5%+
-	elif [[ "$1" == '--opt2' ]]; then
-		brightnessctl set 25%
-	elif [[ "$1" == '--opt3' ]]; then
-		brightnessctl set 5%+
-	elif [[ "$1" == '--opt4' ]]; then
-		gnome-control-center displays
-	fi
+  if [[ "$1" == '--opt1' ]]; then
+    brightnessctl set 5%+
+  elif [[ "$1" == '--opt2' ]]; then
+    brightnessctl set 25%
+  elif [[ "$1" == '--opt3' ]]; then
+    brightnessctl set 5%-
+  elif [[ "$1" == '--opt4' ]]; then
+    env XDG_CURRENT_DESKTOP=GNOME gnome-control-center displays
+  fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $option_1)
-		run_cmd --opt1
-        ;;
-    $option_2)
-		run_cmd --opt2
-        ;;
-    $option_3)
-		run_cmd --opt3
-        ;;
-    $option_4)
-		run_cmd --opt4
-        ;;
+$option_1)
+  run_cmd --opt1
+  ;;
+$option_2)
+  run_cmd --opt2
+  ;;
+$option_3)
+  run_cmd --opt3
+  ;;
+$option_4)
+  run_cmd --opt4
+  ;;
 esac
