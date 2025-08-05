526d2ca86dce4650e83e939389a15278c10b7869
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-05 11:56:53 +0330

    feat(rofi): update prompt and message for root applications

--- rofi/.config/rofi/applets/bin/appasroot.sh
+++ rofi/.config/rofi/applets/bin/appasroot.sh
@@ -5,12 +5,12 @@ source "$HOME"/.config/rofi/applets/shared/theme.bash
 theme="$type/$style"
 
 # Theme Elements
-prompt='Applications'
-mesg='Applications'
+prompt='Applications as root'
+mesg='Select an application to run as root'
 
-	list_col='5'
-	list_row='1'
-	win_width='670px'
+list_col='5'
+list_row='1'
+win_width='670px'
 
 # Options
 
@@ -22,53 +22,70 @@ option_5=""
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "window {width: $win_width;}" \
-		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
+  rofi -theme-str "window {width: $win_width;}" \
+    -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+    -theme-str 'textbox-prompt-colon {str: "";}' \
+    -dmenu \
+    -p "$prompt" \
+    -mesg "$mesg" \
+    -markup-rows \
+    -theme ${theme}
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	polkit_cmd="pkexec env PATH=$PATH DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY"
-	if [[ "$1" == '--opt1' ]]; then
-		${polkit_cmd} ghostty
-	elif [[ "$1" == '--opt2' ]]; then
-		${polkit_cmd} dbus-run-session nautilus
-	elif [[ "$1" == '--opt3' ]]; then
-		${polkit_cmd} code
-	elif [[ "$1" == '--opt4' ]]; then
-		${polkit_cmd} ghostty -e yazi
-	elif [[ "$1" == '--opt5' ]]; then
-		${polkit_cmd} ghostty -e nvim
-	fi
+  # Ensure the necessary environment variables are available
+  local polkit_cmd="pkexec env PATH=\"$PATH\" \
+        DISPLAY=\"$DISPLAY\" \
+        XAUTHORITY=\"$XAUTHORITY\" \
+        WAYLAND_DISPLAY=\"$WAYLAND_DISPLAY\" \
+        HYPRLAND_INSTANCE_SIGNATURE=\"$HYPRLAND_INSTANCE_SIGNATURE\" \
+        XDG_CURRENT_DESKTOP=\"$XDG_CURRENT_DESKTOP\""
+
+  case "$1" in
+  '--opt1')
+    sudo -i /usr/bin/ghostty
+    ;;
+  '--opt2')
+    nautilus admin:///root/
+    ;;
+  '--opt3')
+    ${polkit_cmd} /usr/bin/code
+    ;;
+  '--opt4')
+    ${polkit_cmd} /usr/bin/yazi
+    ;;
+  '--opt5')
+    ${polkit_cmd} /usr/bin/nvim
+    ;;
+  *)
+    echo "Invalid option"
+    return 1
+    ;;
+  esac
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $option_1)
-		run_cmd --opt1
-        ;;
-    $option_2)
-		run_cmd --opt2
-        ;;
-    $option_3)
-		run_cmd --opt3
-        ;;
-    $option_4)
-		run_cmd --opt4
-        ;;
-    $option_5)
-		run_cmd --opt5
-        ;;
+$option_1)
+  run_cmd --opt1
+  ;;
+$option_2)
+  run_cmd --opt2
+  ;;
+$option_3)
+  run_cmd --opt3
+  ;;
+$option_4)
+  run_cmd --opt4
+  ;;
+$option_5)
+  run_cmd --opt5
+  ;;
 esac
