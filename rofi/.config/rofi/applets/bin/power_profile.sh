b31d0efe24b8913fc720c9caa26a1cd231547355
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-10 10:01:46 +0330

    feat(rofi): add power_profile.sh script

---
+++ rofi/.config/rofi/applets/bin/power_profile.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Import Current Theme
+source "$HOME"/.config/rofi/applets/shared/theme.bash
+theme="${type}/${style}"
+
+# Battery Info
+charging_status="$(acpi -b | cut -d',' -f1 | cut -d':' -f2 | tr -d ' ')"
+status="$(powerprofilesctl get)"
+percentage="$(acpi -b | cut -d',' -f2 | tr -d ' ',\%)"
+time="$(acpi -b | cut -d',' -f3)"
+
+if [[ -z "$time" ]]; then
+  time=' Fully Charged'
+fi
+
+# Theme Elements
+prompt="$status"
+mesg="${charging_status}, ${percentage}%, ${time}"
+
+list_col='3'
+list_row='1'
+win_width='400px'
+
+# Options
+option_1="󰌪"
+option_2=""
+option_3="󰓅"
+
+# Rofi CMD
+rofi_cmd() {
+  rofi -theme-str "window {width: $win_width;}" \
+    -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+    -theme-str "textbox-prompt-colon {str: \"$ICON_DISCHRG\";}" \
+    -dmenu \
+    -p "$prompt" \
+    -mesg "$mesg" \
+    -markup-rows \
+    -theme "${theme}"
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+  echo -e "$option_1\n$option_2\n$option_3\n" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+  if [[ "$1" == '--opt1' ]]; then
+    powerprofilesctl set power-saver
+    notify-send -u low "Power profile : ${option_1} power-saver"
+  elif [[ "$1" == '--opt2' ]]; then
+    powerprofilesctl set balanced
+    notify-send -u low "Power profile : ${option_2} balanced"
+  elif [[ "$1" == '--opt3' ]]; then
+    powerprofilesctl set performance
+    notify-send -u low "Power profile : ${option_3} performance"
+  fi
+
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+$option_1)
+  run_cmd --opt1
+  ;;
+$option_2)
+  run_cmd --opt2
+  ;;
+$option_3)
+  run_cmd --opt3
+  ;;
+esac
