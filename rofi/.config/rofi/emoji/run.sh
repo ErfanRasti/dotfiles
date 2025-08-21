9ab51c1d3d6eee44287e7c88f75e50ebfebf2b80
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-21 20:22:30 +0330

    feat(rofi): switch from rofi-emoji to rofimoji

--- rofi/.config/rofi/emoji/run.sh
+++ rofi/.config/rofi/emoji/run.sh
@@ -2,4 +2,5 @@
 
 style=~/.config/rofi/emoji/style.rasi
 
-rofi -modi emoji -show emoji -theme ${style} -emoji-format '{emoji}'
+# rofi -modi emoji -show emoji -theme ${style} -emoji-format '{emoji}'
+rofimoji --selector-args="-theme $style" --hidden-descriptions
