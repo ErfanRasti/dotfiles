edee53c53b8e867038273a0cd3258dc52c1ffe56
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-09-17 14:41:21 +0330

    feat(hyprland): add pyprland to reload script

--- hypr/.config/hypr/scripts/reload_config.sh
+++ hypr/.config/hypr/scripts/reload_config.sh
@@ -2,7 +2,17 @@
 
 waypaper --restore
 
-pkill waybar
+# Restart waybar if running
+if pgrep -x waybar >/dev/null; then
+  pkill waybar
+fi
 waybar &
 
+# Restart pyprland if running
+if pgrep -x pypr >/dev/null; then
+  pkill pypr
+fi
+pypr &
+
+# Apply spicetify
 spicetify apply
