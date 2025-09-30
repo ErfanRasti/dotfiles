de7868a30115c818f7d1425fd4612787e8c4de32
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-09-30 17:51:22 +0330

    feat(swaync): add some options

--- swaync/.config/swaync/scripts/toggle_hypridle.sh
+++ swaync/.config/swaync/scripts/toggle_hypridle.sh
@@ -3,10 +3,10 @@
 if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
   systemctl --user stop hypridle.service
   systemctl --user start hypridle-coffee.service
-  notify-send -u "critical" -a "swaync" "󰅶 Coffee mode enabled" "The screen won't turn off"
+  notify-send -u "critical" -a "swaync" "󰅶 Coffee mode enabled" "The device won't suspend"
 
 else
   systemctl --user stop hypridle-coffee.service
   systemctl --user start hypridle.service
-  notify-send -u "critical" -a "swaync" "󰾪 Coffee mode disabled" "The screen will turn off"
+  notify-send -u "critical" -a "swaync" "󰾪 Coffee mode disabled" "The device will suspend"
 fi
