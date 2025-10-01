7ef4eea7e1a661100778b28aa9e7588fe24612e6
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-01 15:50:40 +0330

    feat(swaync): add toggle wifi script

---
+++ swaync/.config/swaync/scripts/toggle_wifi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set +e # disable immediate exit on error
+
+if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
+  { nmcli radio wifi on; } >/dev/null 2>&1 || :
+else
+  { nmcli radio wifi off; } >/dev/null 2>&1 || :
+
+fi
+
+exit 0
