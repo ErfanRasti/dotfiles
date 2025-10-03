3d084835620e64d3874c86ad3691c598176cb85b
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-04 00:35:42 +0330

    feat(swaync): add hypridle config variable to systemd

--- swaync/.config/swaync/scripts/toggle_hypridle.sh
+++ swaync/.config/swaync/scripts/toggle_hypridle.sh
@@ -3,12 +3,12 @@ set +e # disable immediate exit on error
 
 if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
   {
-    export HYPRIDLE_CONFIG=~/.config/hypr/hypridle-coffee.conf
+    systemctl --user set-environment HYPRIDLE_CONFIG="$HOME/.config/hypr/hypridle-coffee.conf"
     notify-send -u "critical" -a "swaync" "󰅶 Coffee mode enabled" "The device won't suspend"
   } >/dev/null 2>&1 || :
 else
   {
-    export HYPRIDLE_CONFIG=~/.config/hypr/hypridle.conf
+    systemctl --user set-environment HYPRIDLE_CONFIG="$HOME/.config/hypr/hypridle.conf"
     notify-send -u "critical" -a "swaync" "󰾪 Coffee mode disabled" "The device will suspend"
   } >/dev/null 2>&1 || :
 fi
