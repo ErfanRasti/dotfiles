d589556c146b4193c17af6c73fd0966e2b288ff5
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-05 21:59:41 +0330

    feat(matugen): add gtk3 post-hook script

---
+++ matugen/.config/matugen/scripts/gtk3_post_hook.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set +e # ignore the script if it raises an error
+
+gsettings set org.gnome.desktop.interface gtk-theme ""
+
+if grep -q '^post_command.*dark' ~/.config/waypaper/config.ini; then
+  gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+else
+  gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+fi
+
+~/.config/gtk-3.0/scripts/generate_bookmarks.sh ~/.config/gtk-3.0/bookmarks-template
