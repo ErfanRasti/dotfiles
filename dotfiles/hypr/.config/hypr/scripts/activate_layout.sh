1681b7459e338a3964b1fcaa0d67d26b921649ef
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-17 14:32:09 +0330

    feat(hypr/scripts): add hy3 layout

--- dotfiles/hypr/.config/hypr/scripts/activate_layout.sh
+++ dotfiles/hypr/.config/hypr/scripts/activate_layout.sh
@@ -2,19 +2,19 @@
 
 # Check if an argument was provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 [gamemode|smartgaps|dwindle|master|scrolling]"
+  echo "Usage: $0 [gamemode|smartgaps|dwindle|master|scrolling|hy3]"
   exit 1
 fi
 
 case "$1" in
 gamemode)
   ~/.config/hypr/scripts/toggle_gamemode.sh
-  # Do not add `hyprctl reload` to have both smartgaps and gamemode together
+  # Do not add `hyprctl reload` to have this feature with other layouts
   notify-send -u critical -a "Hyprland" " 󰊗 Hyprland game mode toggled"
   ;;
 smartgaps)
   ~/.config/hypr/scripts/toggle_smartgaps.sh
-  # Do not add `hyprctl reload` to have both smartgaps and gamemode together
+  # Do not add `hyprctl reload` to have this feature with other layouts
   notify-send -u critical -a "Hyprland" "  Hyprland smart gaps toggled"
   ;;
 dwindle)
@@ -33,7 +33,12 @@ scrolling)
   hyprctl keyword general:layout scrolling
   notify-send -u critical -a "Hyprland" "  Hyprland scrolling layout activated"
   ;;
-
+hy3)
+  hyprctl reload
+  hyprctl keyword source ~/.config/hypr/config/keybindings-hy3.conf
+  hyprctl keyword general:layout hy3
+  notify-send -u critical -a "Hyprland" " 󰲤 Hyprland hy3 layout activated"
+  ;;
 *)
   echo "Invalid option: $1"
   echo "Usage: $0 [gamemode|smartgaps|dwindle|master|scrolling]"
