3d90fd852177596bf5aa7dfa0c6a7aa78e422fe5
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-19 10:50:50 +0330

    fix(swaync): fix bluetooth and wifi toggling

--- dotfiles/swaync/.config/swaync/scripts/toggle_wifi.sh
+++ dotfiles/swaync/.config/swaync/scripts/toggle_wifi.sh
@@ -2,12 +2,12 @@
 set +e # disable immediate exit on error
 
 if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
-  { nmcli radio wifi on; } >/dev/null 2>&1 || :
-else
   {
     rfkill unblock wifi
-    nmcli radio wifi off
+    nmcli radio wifi on
   } >/dev/null 2>&1 || :
+else
+  { nmcli radio wifi off; } >/dev/null 2>&1 || :
 
 fi
 
