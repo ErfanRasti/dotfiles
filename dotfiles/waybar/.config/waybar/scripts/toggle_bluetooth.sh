44fc6e67ee3264acffb6b8a4971bfa38a15c004a
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-19 10:46:52 +0330

    fix(waybar): fix some bluetooth and wifi toggling

--- dotfiles/waybar/.config/waybar/scripts/toggle_bluetooth.sh
+++ dotfiles/waybar/.config/waybar/scripts/toggle_bluetooth.sh
@@ -4,7 +4,10 @@ set +e # disable immediate exit on error
 if bluetoothctl show | grep -q "Powered: yes"; then
   { bluetoothctl power off; } >/dev/null 2>&1 || :
 else
-  { bluetoothctl power on; } >/dev/null 2>&1 || :
+  {
+    rfkill unblock bluetooth
+    bluetoothctl power on
+  } >/dev/null 2>&1 || :
 fi
 
 exit 0
--- dotfiles/waybar/.config/waybar/scripts/toggle_network.sh
+++ dotfiles/waybar/.config/waybar/scripts/toggle_network.sh
@@ -4,7 +4,10 @@ set +e # disable immediate exit on error
 if [ "$(nmcli radio wifi)" = "enabled" ]; then
   { nmcli radio wifi off; } >/dev/null 2>&1 || :
 else
-  { nmcli radio wifi on; } >/dev/null 2>&1 || :
+  {
+    rfkill unblock wifi
+    nmcli radio wifi on
+  } >/dev/null 2>&1 || :
 fi
 
 exit 0
