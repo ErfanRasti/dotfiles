340a47193aded4a527604595fbb8ff58c003a5f1
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-26 21:35:56 +0330

    feat(swaync): add gsettings to toggle dark mode

--- dotfiles/swaync/.config/swaync/scripts/toggle_darkmode.sh
+++ dotfiles/swaync/.config/swaync/scripts/toggle_darkmode.sh
@@ -2,9 +2,15 @@
 set +e # disable immediate exit on error
 
 if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
-  { sed -i 's/-m "light"/-m "dark"/' ~/.config/waypaper/config.ini; } >/dev/null 2>&1 || :
+  {
+    sed -i 's/-m "light"/-m "dark"/' ~/.config/waypaper/config.ini
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+  } >/dev/null 2>&1 || :
 else
-  { sed -i 's/-m "dark"/-m "light"/' ~/.config/waypaper/config.ini; } >/dev/null 2>&1 || :
+  {
+    sed -i 's/-m "dark"/-m "light"/' ~/.config/waypaper/config.ini
+    gsettings set org.gnome.desktop.interface color-scheme 'default'
+  } >/dev/null 2>&1 || :
 fi
 { setsid -f waypaper --restore; } >/dev/null 2>&1 || :
 
