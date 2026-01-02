b62a40e3ccf24b90957469f0190ab1b2f049d1a2
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-01-03 00:46:33 +0330

    feat(hyprland): update reload bar for new shells

--- dotfiles/hypr/.config/hypr/scripts/reload_bar.sh
+++ dotfiles/hypr/.config/hypr/scripts/reload_bar.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/env bash
 
-AUTOSTART_FILE="$HOME/.config/hypr/config/autostart.conf"
+CONFIG_FILE="$HOME/.config/hypr/hyprland.conf"
+CONFIG_AUTOSTART_FILE="$HOME/.config/hypr/config/autostart.conf"
 
-# Restart waybar if set
-if grep -q "^exec-once = waybar" "$AUTOSTART_FILE"; then
-  echo "Found waybar exec-once"
-  if pgrep -x waybar >/dev/null; then
-    pkill waybar
+# Restart noctalia
+if grep -q "^source = ~/.config/hypr/config/noctalia.conf" "$CONFIG_FILE"; then
+  if pgrep -fx "qs -c noctalia-shell"; then
+    pkill -fx "qs -c noctalia-shell"
   fi
-  setsid -f waybar >/dev/null 2>&1
+  qs -c noctalia-shell &
+  disown
 
-fi
-
-# Restart ashell if set
-if grep -q "^exec-once = ashell" "$AUTOSTART_FILE"; then
-  echo "Found ashell exec-once"
-  if pgrep -x ashell >/dev/null; then
-    pkill ashell
+# Restart dms
+elif grep -q "^source = ~/.config/hypr/config/dms.conf" "$CONFIG_FILE"; then
+  if pgrep -x "dms"; then
+    dms restart
+  else
+    dms run
   fi
-  setsid -f ashell >/dev/null 2>&1
+else
+  # Restart waybar if set
+  if grep -q "^exec-once = waybar" "$CONFIG_AUTOSTART_FILE"; then
+    echo "Found waybar exec-once"
+    if pgrep -x waybar >/dev/null; then
+      pkill waybar
+    fi
+    setsid -f waybar >/dev/null 2>&1
+  fi
 
+  # Restart ashell if set
+  if grep -q "^exec-once = ashell" "$CONFIG_AUTOSTART_FILE"; then
+    echo "Found ashell exec-once"
+    if pgrep -x ashell >/dev/null; then
+      pkill ashell
+    fi
+    setsid -f ashell >/dev/null 2>&1
+  fi
 fi
