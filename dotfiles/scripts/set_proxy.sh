088334240a068d9972ce8de11a29e74aab10fb5b
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-03-09 17:56:06 +0330

    feat: add some options

--- dotfiles/scripts/set_proxy.sh
+++ dotfiles/scripts/set_proxy.sh
@@ -16,7 +16,8 @@ apply_gnome_proxy() {
     export https_proxy="$url"
     export ftp_proxy="$url"
     export socks_proxy="$url"
-    export no_proxy="localhost,127.0.0.1"
+    export no_proxy=""
+    # export no_proxy="localhost,127.0.0.1"
     # export no_proxy="localhost,  127.0.0.0/8,  ::1"
   elif [[ "$proxy_mode" == "'none'" ]]; then
     unset http_proxy https_proxy ftp_proxy socks_proxy no_proxy
