df0deb2d2e85085b015f708fe1f2701d6fed775f
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-05-06 00:27:02 +0330

    feat(rofi/applets): add persistent player menu

--- dotfiles/rofi/.config/rofi/applets/bin/player.sh
+++ dotfiles/rofi/.config/rofi/applets/bin/player.sh
@@ -1,113 +1,130 @@
 #!/usr/bin/env bash
 
-# Import Current Theme
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+modi_script="$0 --modi"
+
+# Import Current Theme
 . "$script_dir/../shared/theme.sh"
-theme="$type/$style"
 
-# Theme Elements
-status="$(playerctl -p spotify status)"
-mesg=$("$HOME"/.config/hypr/scripts/song_detail.sh)
+run_rofi_modi() {
 
-list_col='6'
-list_row='1'
+  get_info() {
+    # Find the current player
+    ~/.config/rofi/applets/bin/current_player.sh
 
-# Options
-if [[ ${status} == "Playing" ]]; then
-  option_1=""
-else
-  option_1=""
-fi
-option_2=""
-option_3="󰙣"
-option_4="󰙡"
-option_5="󰒞"
-option_6="󰑗"
+    current_player=$(cat "/tmp/last_player" 2>/dev/null)
 
-# Toggle Actions
-active=""
-urgent=""
+    # Theme Elements
+    status="$(playerctl -p "$current_player" status)"
+    mesg=$("$HOME"/.config/hypr/scripts/song_detail.sh)
 
-# Random
-random_status=$(playerctl -p spotify shuffle)
-if [[ ${random_status} == "On" ]]; then
-  option_5=""
-  active="-a 4"
-elif [[ ${random_status} == "Off" ]]; then
-  option_5="󰒞"
-  urgent="-u 4"
-fi
+    # Options
+    if [[ ${status} == "Playing" ]]; then
+      option_1=""
+    else
+      option_1=""
+    fi
 
-# Repeat
-repeat_status=$(playerctl -p spotify loop)
-repeat_next_state="Playlist"
-if [[ ${repeat_status} == "Track" ]]; then
-  option_6="󰑘"
-  [ -n "$active" ] && active+=",5" || active="-a 5"
-  repeat_next_state="Playlist"
-elif [[ ${repeat_status} == "Playlist" ]]; then
-  option_6=""
-  repeat_next_state="None"
-elif [[ ${repeat_status} == "None" ]]; then
-  option_6="󰑗"
-  [ -n "$urgent" ] && urgent+=",5" || urgent="-u 5"
-  repeat_next_state="Track"
-fi
+    option_2=""
+    option_3="󰙣"
+    option_4="󰙡"
+    option_5=""
+    option_6="󰑗"
+
+    repeat_status=$(playerctl -p "$current_player" loop)
+    repeat_next_state="None"
+    if [[ ${repeat_status} == "None" ]]; then
+      option_6="󰒞"
+      repeat_next_state="Track"
+    elif [[ ${repeat_status} == "Track" ]]; then
+      option_6="󰑘"
+      repeat_next_state="Playlist"
+    elif [[ ${repeat_status} == "Playlist" ]]; then
+      option_6=""
+      repeat_next_state="None"
+    fi
+  }
+
+  run_cmd() {
+    case "$1" in
+    opt1) playerctl -p "$current_player" play-pause >/dev/null ;;
+    opt2) playerctl -p "$current_player" stop >/dev/null ;;
+    opt3) playerctl -p "$current_player" previous >/dev/null ;;
+    opt4) playerctl -p "$current_player" next >/dev/null ;;
+    opt5) playerctl -p "$current_player" shuffle "Toggle" >/dev/null ;;
+    opt6) playerctl -p "$current_player" loop "$repeat_next_state" >/dev/null ;;
+    esac
+  }
+
+  print_menu() {
+    get_info
+
+    printf '\0message\x1f%s\n' "$mesg"
+    printf '\0markup-rows\x1ftrue\n'
+    printf '\0keep-selection\x1ftrue\n'
+    printf '\0keep-filter\x1ftrue\n'
+
+    # rows
+    if [[ ${status} == "Playing" ]]; then
+      printf '%s\0info\x1fopt1\x1factive\x1ftrue\n' "$option_1"
+    else
+      printf '%s\0info\x1fopt1\n' "$option_1"
+    fi
+
+    printf '%s\0info\x1fopt2\n' "$option_2"
+    printf '%s\0info\x1fopt3\n' "$option_3"
+    printf '%s\0info\x1fopt4\n' "$option_4"
+
+    random_status=$(playerctl -p "$current_player" shuffle)
+    if [[ ${random_status} == "On" ]]; then
+      printf '%s\0info\x1fopt5\x1factive\x1ftrue\n' "$option_5"
+    else
+      printf '%s\0info\x1fopt5\n' "$option_5"
+    fi
+
+    if [[ ${repeat_status} == "Track" || ${repeat_status} == "Playlist" ]]; then
+      printf '%s\0info\x1fopt6\x1factive\x1ftrue\n' "$option_6"
+    elif [[ ${repeat_status} == "None" ]]; then
+      printf '%s\0info\x1fopt6\n' "$option_6"
+    else
+      printf '%s\0info\x1fopt6\x1furgent\x1ftrue\n' "$option_6"
+    fi
+  }
+
+  # https://man.archlinux.org/man/rofi-script.5.en
+  case "$ROFI_RETV" in
+  0) ;;
+  1)
+    # This line is needed to generate the variables for `run_cmd`
+    get_info
+
+    run_cmd "$ROFI_INFO"
+    ;;
+  esac
+
+  print_menu
+}
 
 # Rofi CMD
 rofi_cmd() {
-  # shellcheck disable=SC2086
-  rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+  list_col='6'
+  list_row='1'
+  win_width='700px'
+
+  rofi \
+    -show volume \
+    -modi "volume:${modi_script}" \
+    -theme-str "window { width: $win_width; }" \
+    -theme-str "listview { columns: $list_col; lines: $list_row; }" \
     -theme-str 'textbox-prompt-colon {str: "";}' \
-    -dmenu \
-    -p "$status" \
-    -mesg "$mesg" \
-    ${active} ${urgent} \
-    -markup-rows \
     -theme "${theme}"
 }
 
-# Pass variables to rofi dmenu
-run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
-}
-
-# Execute Command
-run_cmd() {
-  if [[ "$1" == '--opt1' ]]; then
-    playerctl -p spotify play-pause
-  elif [[ "$1" == '--opt2' ]]; then
-    playerctl -p spotify stop
-  elif [[ "$1" == '--opt3' ]]; then
-    playerctl -p spotify previous
-  elif [[ "$1" == '--opt4' ]]; then
-    playerctl -p spotify next
-  elif [[ "$1" == '--opt5' ]]; then
-    playerctl -p spotify shuffle "Toggle"
-  elif [[ "$1" == '--opt6' ]]; then
-    playerctl -p spotify loop "${repeat_next_state}"
-  fi
-}
-
-# Actions
-chosen="$(run_rofi)"
-case ${chosen} in
-"$option_1")
-  run_cmd --opt1
-  ;;
-"$option_2")
-  run_cmd --opt2
-  ;;
-"$option_3")
-  run_cmd --opt3
-  ;;
-"$option_4")
-  run_cmd --opt4
-  ;;
-"$option_5")
-  run_cmd --opt5
-  ;;
-"$option_6")
-  run_cmd --opt6
-  ;;
-esac
+if [[ -z "$1" ]]; then
+  rofi_cmd
+elif [[ "$1" == "--modi" ]]; then
+  run_rofi_modi
+else
+  echo "Error: unknown argument '$1'" >&2
+  exit 1
+fi
