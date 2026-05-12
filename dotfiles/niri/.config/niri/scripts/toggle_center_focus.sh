9a084a956fa27ce2ea091e45c338b8cb4d87b1cf
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-05-12 19:50:59 +0330

    fix(niri/scripts): create default file for toggle center focus

--- dotfiles/niri/.config/niri/scripts/toggle_center_focus.sh
+++ dotfiles/niri/.config/niri/scripts/toggle_center_focus.sh
@@ -3,8 +3,15 @@
 config="$HOME/.config/niri/config/overwrites.kdl"
 
 mkdir -p "$(dirname "$config")"
+touch "$config"
 
-if grep -q 'center-focused-column "on-overflow"' "$config"; then
+if ! grep -q 'center-focused-column' "$config"; then
+  cat >>"$config" <<EOF
+layout {
+  center-focused-column "always"
+}
+EOF
+elif grep -q 'center-focused-column "on-overflow"' "$config"; then
   sed -i 's/center-focused-column "on-overflow"/center-focused-column "always"/' "$config"
 else
   sed -i 's/center-focused-column "always"/center-focused-column "on-overflow"/' "$config"
