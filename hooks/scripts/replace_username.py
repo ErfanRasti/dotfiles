9d3254c6fbc1279579c154beeb9866ba6fc6ba13
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-07-08 11:50:38 +0330

    feat(hooks): add hostname pattern

--- hooks/scripts/replace_username.py
+++ hooks/scripts/replace_username.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import os
 import sys
+import socket
 
 username = os.getenv("USER")
+hostname = socket.gethostname()
 
 pattern1 = f"/home/{username}"
 replacement1 = "~"
 
 pattern2 = f"{username}"
 replacement2 = "user"
+
+pattern3 = f"{hostname}"
+replacement3 = "host"
 
 for path in sys.argv[1:]:
     if not os.path.isfile(path):
@@ -18,15 +23,8 @@ for path in sys.argv[1:]:
         content = f.read()
 
     new_content = content.replace(pattern1, replacement1)
-
-    if new_content != content:
-        with open(path, "w", encoding="utf-8") as f:
-            f.write(new_content)
-
-    with open(path, "r", encoding="utf-8", errors="ignore") as f:
-        content = f.read()
-
     new_content = content.replace(pattern2, replacement2)
+    new_content = content.replace(pattern3, replacement3)
 
     if new_content != content:
         with open(path, "w", encoding="utf-8") as f:
