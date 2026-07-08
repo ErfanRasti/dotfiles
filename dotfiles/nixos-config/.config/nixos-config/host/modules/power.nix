07d6214bcfd786d592590513f5df265408b998a5
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-07-08 12:16:26 +0330

    feat(nixos-config/host): add power.nix

---
+++ dotfiles/nixos-config/.config/nixos-config/host/modules/power.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+let
+  postPowertop = pkgs.writeShellScript "post-powertop" ''
+    log() { echo "[usb-bt-wakeup] $*"; }
+
+    # Don't suspend plugged-in USB input devices
+    for f in $(find /sys/bus/usb/drivers/usbhid -regex '.*\/[0-9:.-]+' -printf '%f\n' | cut -d ":" -f 1 | sort -u); do
+      echo on >|"/sys/bus/usb/devices/$f/power/control"
+    done
+
+    # Auto-detect Bluetooth USB devices and enable system wakeup
+    for prod in /sys/bus/usb/devices/*/product; do
+      dev=''${prod%/product}
+      [[ "$dev" == *:* ]] && continue
+
+      product=$(cat "$prod" 2>/dev/null)
+      [[ "$product" != *[Bb]luetooth* ]] && continue
+
+      wakeup="$dev/power/wakeup"
+      if [ -w "$wakeup" ]; then
+        echo 'enabled' >"$wakeup"
+        log "Enabled wakeup for $product"
+      fi
+    done
+  '';
+in
+{
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+  services.power-profiles-daemon.enable = true;
+  powerManagement.powertop.enable = true;
+
+  systemd.services.powertop.serviceConfig.ExecStartPost = [ "${postPowertop}" ];
+
+  # Suspend first then hibernate when closing the lid
+  services.logind.settings.Login.LidSwitch = "suspend";
+  # Hibernate on power button pressed
+  services.logind.settings.Login.PowerKey = "suspend";
+  services.logind.settings.Login.PowerKeyLongPress = "poweroff";
+}
