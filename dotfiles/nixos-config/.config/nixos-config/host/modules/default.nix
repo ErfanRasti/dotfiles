ee21a828a13acaf810d7f9ddfe2498fdd520b76f
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-07-08 12:17:01 +0330

    feat(nixos-config/host): add default.nix

---
+++ dotfiles/nixos-config/.config/nixos-config/host/modules/default.nix
@@ -0,0 +1,19 @@
+{
+  imports = [
+    ./nix.nix # Nix daemon settings (gc, optimise, experimental-features)
+    ./boot.nix # Boot config
+    ./kernel.nix # Kernel packages (needed by nvidia)
+    ./inputs.nix # Touchpad etc (lowest priority)
+    ./networks.nix # Networking, hostname, firewall
+    ./nvidia.nix # GPU drivers (depends on boot.kernelPackages)
+    ./services.nix # Desktop, display manager, xserver
+    ./audio.nix # Audio hardware
+    ./security.nix # Sudo, security policies
+    ./users.nix # User accounts
+    ./system.nix # System state version
+    ./packages.nix # System packages, programs, unfree
+    ./swap.nix # Swap partition
+    ./power.nix # Poewr management
+    ./fonts.nix # Font packages
+  ];
+}
