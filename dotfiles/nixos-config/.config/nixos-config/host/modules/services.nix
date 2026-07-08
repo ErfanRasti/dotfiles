2d46e0189e8fdd4e0a00154b96c559250cd1ab27
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2026-07-08 12:16:39 +0330

    feat(nixos-config/host): add services.nix

---
+++ dotfiles/nixos-config/.config/nixos-config/host/modules/services.nix
@@ -0,0 +1,26 @@
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = false;
+
+  # Enable the GNOME Desktop Environment.
+  # services.displayManager.gdm.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "matrix";
+      battery_id = "BAT0";
+      bigclock = "en";
+    };
+  };
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  #
+
+  services.hardware.openrgb.enable = true;
+}
